28c54fcd6a72e067fd4105b04907c5fc79115f11
Eli Sagy <sagyeli@gmail.com>
2017-05-08 11:04:53 +0300

    Setting production MongoDB connection string

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,6 +1,9 @@
 'use strict';
 /*eslint no-process-env:0*/
 
+import fs from 'fs'
+var atlasCredentials = JSON.parse(fs.readFileSync('../apis.key.json', 'utf8'))['atlasCredentials']
+
 // Production specific configuration
 // =================================
 module.exports = {
@@ -19,6 +22,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/bitmeetapi'
+      || 'mongodb://' + atlasCredentials + '@bitmeet-shard-00-00-z90zh.mongodb.net:27017,bitmeet-shard-00-01-z90zh.mongodb.net:27017,bitmeet-shard-00-02-z90zh.mongodb.net:27017/bitmeetapi?ssl=true&replicaSet=Bitmeet-shard-0&authSource=admin'
   }
 };
